a1b4ec21b947a15544dd129b06548782a92ff397
Vinicius Estevam <estevamvinicius31@gmail.com>
2021-06-22 10:34:38 -0300

    Create Front End challenge

---
+++ README.md
@@ -0,0 +1,60 @@
+![image](https://user-images.githubusercontent.com/40845824/121069742-3accdb00-c7a4-11eb-87d0-3dc47e433762.png)
+
+
+# 🚀 Front end challenge
+
+Bem-vindo(a). Este é o desafio Front end!
+
+O objetivo deste desafio é avaliar suas habilidades de programação.
+Quando sua solução estiver pronta, basta responder o e-mail que recebeu com o link do seu repo aqui no Github!
+Em seguida, enviaremos o feedback e as instruções dos próximos passos!
+
+Caso tenha alguma dúvida, nós estamos disponíveis no email *recrutamento@rocketseat.com.br*
+Bom desafio!
+
+> ⚠️ **É importante que o seu repo esteja público, caso contrário não iremos conseguir avaliar sua resposta**
+
+# 🧠 Contexto
+
+![Protótipo](https://storage.googleapis.com/xesque-dev/challenge-images/prototipo.png?42)
+
+A Rocketseat está criando uma loja de camisetas/canecas (pensar no texto)... O desafio será implementar um e-commerce que deverá ter as seguintes funcionalidades:
+- [ ] Catálogo de produtos com paginação
+- [ ] Filtragem produtos por categoria
+- [ ] Busca por nome do produto
+- [ ] Adicionar e remover produtos do carrinho
+- [ ] Finalizar compra
+
+## 📋 Instruções
+
+Chegou a hora de colocar a mão na massa!
+
+- Siga [esse protótipo](https://www.figma.com/file/rET9F2CeUEJdiVN7JRu993/E-commerce---capputeeno?node-id=680%3A6449)
+- Utilize Next.js e Typescript
+- Faça a estilização com styled-components
+- Utilize o local storage para gerenciar o carrinho
+- Por favor, inclua no README as instruções de instalação do projeto
+- Sinta-se livre para incluir quaisquer observações
+- Consuma nossa **API GraphQL** rodando o comando `yarn start` na pasta `api`
+
+## ✔️ Critérios de Avaliação
+
+Além dos requisitos levantados acima, iremos olhar para os seguintes critérios durante a correção do desafio:
+
+- Gerenciamento de estado
+- Responsividade
+- Preocupação com usabilidade
+- Preocupação com acessibilidade
+- Testes e2e
+- Padrão de commits (_Conventional_)
+
+## 😎 Seria legal
+- Fazer deploy na vercel ou em outro local de sua preferência e disponibilizar um link de visualização
+- Testes unitários
+- Utilização de cache do Next.js
+
+---
+
+_O desafio acima foi cuidadosamente construído para propósitos de avaliação apenas. Já possuimos uma funcionalidade similar na nossa plataforma._
+
+Made with 💜 at Rocketseat
---
+++ api/db.js
@@ -0,0 +1,32 @@
+const faker = require('faker');
+
+const TOTAL_PAGES = 5;
+
+const baseProducts = [
+  { name: 'Caneca de cerâmica rústica', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/caneca-06.jpg', category: 'mugs' },
+  { name: 'Camiseta not today.', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-05.jpg', category: 't-shirts' },
+  { name: 'Caneca Black Ring', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/caneca-04.jpg', category: 'mugs' },
+  { name: 'Camiseta Broken Saints', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-03.jpg', category: 't-shirts' },
+  { name: 'Camiseta Outcast', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-06.jpg', category: 't-shirts' },
+  { name: 'Caneca The Grounds', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/caneca-05.jpg', category: 'mugs' },
+  { name: 'Camiseta evening', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-02.jpg', category: 't-shirts' },
+  { name: 'Caneca preto fosco', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/caneca-01.jpg', category: 'mugs' },
+  { name: 'Caneca Never settle', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/caneca-03.jpg', category: 'mugs' },
+  { name: 'Camiseta DREAMER', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-01.jpg', category: 't-shirts' },
+  { name: 'Caneca Decaf! P&Co', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/caneca-02.jpg', category: 'mugs' },
+  { name: 'Camiseta Ramones', description: faker.lorem.paragraph(), image_url: 'https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-04.jpg', category: 't-shirts' },
+]
+
+const allProducts = new Array(TOTAL_PAGES).fill(1).reduce((acc) => {
+  const products = baseProducts.map(product => ({
+    ...product, 
+    id: faker.datatype.uuid(),
+    price: faker.commerce.price(20, 100)
+  })).sort(() => .5 - Math.random());
+
+  return [...acc, ...products]
+}, [])
+
+module.exports = {
+  products: allProducts
+}
