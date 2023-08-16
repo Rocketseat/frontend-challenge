f8c89194b3e83176ca1ce2c466436d18b2c8133f
mynamegitrepos <samuelpereiradesouza665@gmail.com>
2023-08-16 00:05:43 -0300

    feat: first commit

---
+++ capputeeno/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  )
+}
