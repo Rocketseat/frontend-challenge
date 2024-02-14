9c00b6def7554233c87ea9314da459071fca4516
Renan.T <rtoniolo4@gmail.com>
2024-02-14 14:24:16 -0300

    refactor: :truck: move icons to separate folder

--- capputeeno/src/components/input-primary.tsx
+++ capputeeno/src/components/input-primary.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import SearchIcon from "./seach-icon";
 import { InputHTMLAttributes } from "react";
 
 export const InputPrimary = styled.input`
@@ -28,6 +27,35 @@ const InputContanier = styled.div`
     transform: translateY(-50%);
   }
 `;
+
+export function SearchIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="11.059"
+        cy="11.0586"
+        r="7.06194"
+        stroke="#737380"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.0034 20.0033L16.0518 16.0517"
+        stroke="#737380"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
 
 export function InputPrimaryWithSeach(
   props: InputHTMLAttributes<HTMLInputElement>
