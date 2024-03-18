12945d31ac32a5378f94c01946f059c7c82f7fe4
Keila Amada <keila.amada@truckpag.com.br>
2024-03-17 21:33:53 -0300

    feat: installing dependencies

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Teste</main>;
+  return <main className={styles.main}>keila</main>;
 }
